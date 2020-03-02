44e3ae0e403aa2e66a489c1ec48c6e820e2f94dc
Dasha Akhten <dasha@Dashas-iMac.attlocal.net>
2020-03-02 13:54:54 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import STORE from "./store";
+
+import App from "./App";
+
+ReactDOM.render(<App store={STORE} />, document.getElementById("root"));
